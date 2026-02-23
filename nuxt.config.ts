38fd0f03aa28ba856803012946d216db99eb8fab
Nicholas Braun <hallo@nicholasbraun.de>
2026-02-23 09:57:42 +0100

    feat: add plausible analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,15 +13,39 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
         { rel: "manifest", href: "/site.webmanifest" },
         {
           rel: "alternate",
           type: "application/rss+xml",
           title: "RSS Feed",
           href: "/rss.xml",
+        },
+      ],
+      script: [
+        {
+          async: true,
+          src: "https://plausible.io/js/pa-AOIR4PhGlK7-HIkuq6ISs.js",
+        },
+        {
+          innerHTML:
+            "window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()",
         },
       ],
     },
