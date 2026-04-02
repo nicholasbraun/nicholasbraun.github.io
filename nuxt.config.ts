d03dde824f35baa9e7739b9709afc35172e33fa1
Nicholas Braun <hallo@nicholasbraun.de>
2026-04-02 23:09:02 +0200

    chore: update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,7 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: "preconnect", href: "https://plausible.io" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
         {
           rel: "icon",
