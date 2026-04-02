901ef5c411ac6ce2d392ca45e8e6ff18d83bf586
Nicholas Braun <hallo@nicholasbraun.de>
2026-04-02 18:20:00 +0200

    feat: move tags to the side on desktop

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -49,42 +49,43 @@ useSeoMeta({
 </script>
 
 <template>
-  <ContentRenderer v-if="page" :value="page" />
+  <article class="content content--blog">
+    <ContentRenderer v-if="page" :value="page" class="intro" />
 
-  <nav v-if="allTags.length" aria-label="Blog tags">
-    <p>Tags:</p>
-    <ul class="tags__list">
-      <li class="tags__list-item">
-        <NuxtLink :to="{ path: '/blog' }">All</NuxtLink>
-      </li>
-      <li v-for="tag in allTags" :key="tag" class="tags__list-item">
-        <NuxtLink :to="{ path: '/blog', query: { tag } }">{{ tag }}</NuxtLink>
-      </li>
-    </ul>
-    <p v-if="activeTag">Filtering by: {{ activeTag }}</p>
-  </nav>
+    <aside v-if="allTags.length" class="tags" aria-label="Blog tags">
+      <p>Tags:</p>
+      <ul class="tags__list">
+        <li class="tags__list-item">
+          <NuxtLink :to="{ path: '/blog' }">All</NuxtLink>
+        </li>
+        <li v-for="tag in allTags" :key="tag" class="tags__list-item">
+          <NuxtLink :to="{ path: '/blog', query: { tag } }">{{ tag }}</NuxtLink>
+        </li>
+      </ul>
+      <p v-if="activeTag">Filtering by: {{ activeTag }}</p>
+    </aside>
 
-  <nav>
-    <p>Blog posts:</p>
-    <ul v-if="filteredPosts.length">
-      <li v-for="item in filteredPosts" :key="item.path">
-        <NuxtLink :to="item.path">
-          <time v-if="item.date" :datetime="item.date">
-            {{
-              new Date(item.date).toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })
-            }} </time
-          >:
-          {{ item.title }}
-        </NuxtLink>
-        <span v-if="item.tags?.length"> ({{ item.tags.join(", ") }}) </span>
-      </li>
-    </ul>
-    <p v-else>No posts found for this tag.</p>
-  </nav>
+    <div class="posts">
+      <h2>Posts</h2>
+      <ul v-if="filteredPosts.length">
+        <li v-for="item in filteredPosts" :key="item.path">
+          <NuxtLink :to="item.path">
+            <time v-if="item.date" :datetime="item.date">
+              {{
+                new Date(item.date).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })
+              }} </time
+            >:
+            {{ item.title }}
+          </NuxtLink>
+        </li>
+      </ul>
+      <p v-else>No posts found for this tag.</p>
+    </div>
+  </article>
 </template>
 
 <style lang="css" scoped>
@@ -96,5 +97,31 @@ useSeoMeta({
 .tags__list-item {
   display: inline-block;
   margin-right: 10px;
+}
+
+@media screen and (min-width: 900px) {
+  .content {
+    display: grid;
+    grid-template-columns: 5fr 1fr;
+    grid-template-rows: auto 1fr;
+  }
+
+  .intro {
+    grid-column: 1;
+    grid-row: 1;
+  }
+
+  .posts {
+    grid-column: 1;
+    grid-row: 2;
+  }
+
+  .tags {
+    grid-column: 2;
+    grid-row: 1 / -1;
+  }
+  .tags__list-item {
+    display: block;
+  }
 }
 </style>
